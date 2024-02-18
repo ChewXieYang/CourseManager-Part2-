8f0727331f1138ab1a09bd03b829741412bcba05
Chew Xie Yang <1221304859@student.mmu.edu.my>
2024-02-18 23:57:04 +0800

    Update Courses.java

--- Courses.java
+++ Courses.java
@@ -1,47 +1,98 @@
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class Courses {
     private int Credit;
     private String Code;
-    private Set<String> PreReq;
-    
-    public Courses(int Credit, String Code){
+    private String PreReq;
+    private List<Courses> prerequisites;
+
+    // Constructor, getters, setters
+    public Courses(int Credit, String Code, String PreReq){
         this.Credit = Credit;
         this.Code = Code;
+        this.PreReq = PreReq;
+        this.prerequisites = new ArrayList<>();
     }
-    public Courses(int Credit, String code,String pre1){
-        this.Credit = Credit;
-        this.Code = Code;
-        PreReq.add(pre1);
-    }
-    public Courses(int Credit, String code,String pre1,String pre2){
-        this.Credit = Credit;
-        this.Code = Code;
-        PreReq.add(pre1);
-        PreReq.add(pre2);
-    }
-    
     public int getCredit(){
         return Credit;
     }
     public String getCode(){
         return Code;
     }
-    public Set<String> getPreReq(){
+    public String getPreReq(){
         return PreReq;
     }
-    public void setPrerequire(String course) {
-        PreReq.add(course);
+    public List<Courses> getPrerequisites() {
+        return prerequisites;
     }
-    public void setCredit(int credit) {
-        this.Credit = credit;
+}
+
+
+class CourseFile{
+    public static void main(String[] args){
+    String CourseFile = "Courses.csv";
+
+    try{
+    Scanner scanner = new Scanner(new File(CourseFile));
+    LinkedList<String[]> CourseData = new LinkedList<>();
+
+    while(scanner.hasNextLine()){
+        String[] line = scanner.nextLine().split(",");
+        CourseData.add(line);
     }
-    public void setCode(String code) {
-        this.Code = code;
+
+    for (String[] row: CourseData){
+        for(String value : row){
+            System.out.print(value + "\t");
+        }
+        System.out.println();
+    }
+    scanner.close();
+    } catch(FileNotFoundException e){
+        e.printStackTrace();
+    }
+}
+}
+
+class Course implements Serializable{ //Copy and paste it to Course.java
+    public String id;
+    public String name;
+    private List<Course> prerequisites;
+
+    public Course(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.prerequisites = new ArrayList<>();
+    }
+
+    public Course(String id, String name, List<Course> prerequisites) {
+        this.id = id;
+        this.name = name;
+        this.prerequisites = prerequisites;
+    }
+
+    public void setPrerequisites(List<Course> prerequisites) {
+        this.prerequisites = prerequisites;
     }
 
     public String toString() {
-        return "CourseID: " + Code ;
+        return "CourseID: " + id + ", CourseName: " + name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Course> getPrerequisites() {
+        return prerequisites;
+    }
+
+}
+
 }
 
